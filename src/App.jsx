610b5e0c57793479d8b193f74c3a6c81798df328
PrinceRaval <divya.raval92@gmail.com>
2025-06-13 17:30:10 -0400

    Made the Home Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { LoadingScreen } from "./components/LoadingScreen";
 import "./index.css";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
+import { Home } from "./components/sections/Home";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -19,6 +20,7 @@ function App() {
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
       </div>
     </>
   );
---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,33 @@
+export const Home = () => {
+  return (
+    <section
+      id="home"
+      className="min-h-screen flex items-center justify-center relative"
+    >
+      <div className="text-center z-10 px-4">
+        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent">
+          Hi, I'm Prince Raval
+        </h1>
+        <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+          I'm a full-stack developer who loves to make clean web application, My
+          goal is to Bulid solution that offers Excellent perfromance and
+          wonderful user expercience
+        </p>
+        <div className="flex justify-center space-x-4">
+          <a
+            href="#projects"
+            className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+          >
+            View Projects
+          </a>
+          <a
+            href="#contact"
+            className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+          >
+            Contact Me
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
